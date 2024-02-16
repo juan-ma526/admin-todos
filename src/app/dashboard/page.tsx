6efa441de53b279437f3dfc4bd0b059240f516f1
juan-ma526 <jmperez675@gmail.com>
2024-02-16 17:48:59 -0300

    add next auth with github

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,22 @@
 import { WidgetItem } from "@/components";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
-  return <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">{/*   <WidgetItem /> */}</div>;
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+  return (
+    <div className="grid gap-6 sm:grid-cols-2 grid-cols-1">
+      <WidgetItem title="Usuario Conectado S-side">
+        <div className="flex flex-col">
+          <span>{session.user?.email}</span>
+          <span>{session.user?.name}</span>
+          <span>{session.user?.image}</span>
+        </div>
+      </WidgetItem>
+    </div>
+  );
 }
